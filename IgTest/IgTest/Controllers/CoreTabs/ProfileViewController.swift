40835056f81ebdacb4b246dcb31a34d7019ced66
陳鋐洋 <ahic2569k3ig@gmail.com>
2021-10-12 15:59:01 +0800

    add ProfileVC Header UI

--- IgTest/IgTest/Controllers/CoreTabs/ProfileViewController.swift
+++ IgTest/IgTest/Controllers/CoreTabs/ProfileViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ProfileViewController: UIViewController {
     
     private var collectionView: UICollectionView?
+    
+    private var userPosts = [UserPost]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,7 +31,7 @@ class ProfileViewController: UIViewController {
         layout.itemSize = CGSize(width: size, height: size)
         collectionView = UICollectionView(frame: .zero,
                                           collectionViewLayout: layout)
-        collectionView?.backgroundColor = .red
+        
         
         // cell
         collectionView?.register(PhotoCollectionViewCell.self, forCellWithReuseIdentifier: PhotoCollectionViewCell.identifier)
@@ -78,17 +80,27 @@ extension ProfileViewController: UICollectionViewDelegate, UICollectionViewDataS
         if section == 0 {
             return 0
         }
+        //return userPosts.count
         return 30
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCollectionViewCell.identifier, for: indexPath) as! PhotoCollectionViewCell
+        //let model = userPosts[indexPath.row]
         cell.configure(debug: "test")
+        //cell.configure(with: model)
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         collectionView.deselectItem(at: indexPath, animated: true)
+        
+        // get the model and open post controller
+        //let model = userPosts[indexPath.row]
+        let vc = PostViewController(model: nil)
+        vc.title = "Post"
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
         
     }
     
@@ -112,7 +124,7 @@ extension ProfileViewController: UICollectionViewDelegate, UICollectionViewDataS
         let profileHeader = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
                                                                      withReuseIdentifier: ProfileInfoHeaderCollectionReusableView.identifier,
                                                                      for: indexPath) as! ProfileInfoHeaderCollectionReusableView
-        
+        profileHeader.delegate = self
         return profileHeader
     }
     
@@ -125,4 +137,35 @@ extension ProfileViewController: UICollectionViewDelegate, UICollectionViewDataS
         // size for section tabs
         return CGSize(width: collectionView.width, height: 65)
     }
+}
+
+// MARK: ProfileInfoHeaderCollectionReusableViewDelegate
+
+extension ProfileViewController: ProfileInfoHeaderCollectionReusableViewDelegate {
+    func profileHeaderDidTapPostsButton(_ header: ProfileInfoHeaderCollectionReusableView) {
+        // scroll to the posts
+        collectionView?.scrollToItem(at: IndexPath(row: 0, section: 1), at: .top, animated: true)
+    }
+    
+    func profileHeaderDidTapFollowersButton(_ header: ProfileInfoHeaderCollectionReusableView) {
+        let vc = ListViewController()
+        vc.title = "Followers"
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func profileHeaderDidTapFollowingButton(_ header: ProfileInfoHeaderCollectionReusableView) {
+        let vc = ListViewController()
+        vc.title = "Following"
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func profileHeaderDidTapEditProfileButton(_ header: ProfileInfoHeaderCollectionReusableView) {
+        let vc = EditProfileViewController()
+        vc.title = "Edit Profile"
+        present(UINavigationController(rootViewController: vc), animated: true)
+    }
+    
+    
 }
---
+++ IgTest/IgTest/Controllers/Other/General/ListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ListViewController.swift
+//  IgTest
+//
+//  Created by 陳鋐洋 on 2021/10/12.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+
+}
