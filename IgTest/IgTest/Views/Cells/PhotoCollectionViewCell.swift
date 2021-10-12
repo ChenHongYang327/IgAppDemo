45734fedc54cdf133a788e88b14a52aa62834a1d
陳鋐洋 <ahic2569k3ig@gmail.com>
2021-10-12 13:57:53 +0800

    add profileVC UI

--- IgTest/IgTest/Views/Cells/PhotoCollectionViewCell.swift
+++ IgTest/IgTest/Views/Cells/PhotoCollectionViewCell.swift
@@ -9,4 +9,55 @@ import UIKit
 
 class PhotoCollectionViewCell: UICollectionViewCell {
     static let identifier = "PhotoCollectionViewCell"
+    
+    private let photoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        photoImageView.frame = contentView.bounds
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        photoImageView.image = nil
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        contentView.backgroundColor = .secondarySystemBackground
+        contentView.addSubview(photoImageView)
+        contentView.clipsToBounds = true
+        
+        accessibilityLabel = "User post image"
+        accessibilityHint = "Double-tap to open post"
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public func configure(with model: UserPost){
+        let thumbnailURL = model.thumbnailImage
+        
+        URLSession.shared.dataTask(with: thumbnailURL) { data, _, error in
+            guard let data = data else {
+                return
+            }
+
+            DispatchQueue.main.async {
+                self.photoImageView.image = UIImage(data: data)
+            }
+        }.resume()
+    }
+    
+    public func configure(debug imageName: String){
+        photoImageView.image = UIImage(named: imageName)
+    }
+    
+    
 }
